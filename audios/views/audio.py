46f63a9905908f04e49fbcdf97da49f2aacf1c86
Vladimir Grechukhin <inmosh@protomail.ch>
2021-09-15 21:23:36 +0400

    Add next-previous buttons

--- audios/views/audio.py
+++ audios/views/audio.py
@@ -12,4 +12,25 @@ def audio(request, audio_id, audio_slug):
 
     audios = get_sorted_audios(_audio.directory_id)
 
-    return render(request, 'audios/audio.html', {'audio': _audio, 'audios': audios})
+    previous, _next = None, None
+
+    for audios_list in audios:
+        for i, each in enumerate(audios_list[1]):
+            if each.id != _audio.id:
+                continue
+            if i != 0:
+                previous = audios_list[1][i - 1]
+            try:
+                _next = audios_list[1][i + 1]
+            except IndexError:
+                pass
+            break
+
+    return render(
+        request,
+        'audios/audio.html', {
+            'audio': _audio,
+            'audios': audios,
+            'previous': previous,
+            'next': _next,
+        })
