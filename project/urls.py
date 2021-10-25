e666f6c0fb397c7c202035cbe760010a80f2f82c
Vladimir Grechukhin <inmosh@protomail.ch>
2021-10-25 20:43:40 +0300

    tests

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 
 def webhook(request):
     with open('log.txt', 'a') as f:
         f.write(f'{request.POST}\n\n{request.body}\n\n')
+
+    return HttpResponse('')
 
 
 urlpatterns = [
